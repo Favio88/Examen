87c4b16f901764d6b6fc7e6d8f9eeff0e11cf3fd
Favio Orlando <favio_galarza@hotmail.com>
2019-03-02 17:36:19 -0600

    Terminado con código simplficado

--- app/src/main/java/com/favio/examen/MainActivity.java
+++ app/src/main/java/com/favio/examen/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button btn1, btn2, btn3, btn4, btn5, btn_nuevo;
     TextView txtv_num, txtv_res, txtv_ganador;
     int num=0, res=0, intentos=0;
+    LinearLayout ll_cartas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +31,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         txtv_num=findViewById(R.id.txtv_num);
         txtv_res=findViewById(R.id.txtv_res);
         txtv_ganador=findViewById(R.id.txtv_ganador);
+        ll_cartas=findViewById(R.id.ll_cartas);
 
         btn1.setOnClickListener(this);
         btn2.setOnClickListener(this);
         btn3.setOnClickListener(this);
         btn4.setOnClickListener(this);
         btn5.setOnClickListener(this);
-        btn_nuevo.setOnClickListener(this);
+
+        btn_nuevo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                jugarNuevo();
+            }
+        });
 
         jugarNuevo();
     }
@@ -44,59 +53,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
 
         Random rdm=new Random();
-        int numRdm=rdm.nextInt(10)+1;
+        int numRdm=rdm.nextInt(9)+1;
 
-        switch(v.getId()){
+        Button btn=(Button)v;
 
-            case R.id.btn1:
-                txtv_num.setText(Integer.toString(numRdm));
-                res=numRdm+res;
-                txtv_res.setText(Integer.toString(res));
-                btn1.setBackgroundColor(Color.GREEN);
-                intentos++;
-                btn1.setEnabled(false);
-                verificar();
-                break;
-            case R.id.btn2:
-                txtv_num.setText(Integer.toString(numRdm));
-                res=numRdm+res;
-                txtv_res.setText(Integer.toString(res));
-                btn2.setBackgroundColor(Color.GREEN);
-                intentos++;
-                btn2.setEnabled(false);
-                verificar();
-                break;
-            case R.id.btn3:
-                txtv_num.setText(Integer.toString(numRdm));
-                res=numRdm+res;
-                txtv_res.setText(Integer.toString(res));
-                btn3.setBackgroundColor(Color.GREEN);
-                intentos++;
-                btn3.setEnabled(false);
-                verificar();
-                break;
-            case R.id.btn4:
-                txtv_num.setText(Integer.toString(numRdm));
-                res=numRdm+res;
-                txtv_res.setText(Integer.toString(res));
-                btn4.setBackgroundColor(Color.GREEN);
-                intentos++;
-                btn4.setEnabled(false);
-                verificar();
-                break;
-            case R.id.btn5:
-                txtv_num.setText(Integer.toString(numRdm));
-                res=numRdm+res;
-                txtv_res.setText(Integer.toString(res));
-                btn5.setBackgroundColor(Color.GREEN);
-                intentos++;
-                btn5.setEnabled(false);
-                verificar();
-                break;
-            case R.id.btn_nuevo:
-                jugarNuevo();
-                break;
-        }
+        txtv_num.setText(Integer.toString(numRdm));
+        res=numRdm+res;
+        txtv_res.setText(Integer.toString(res));
+        btn.setBackgroundColor(Color.GREEN);
+        intentos++;
+        btn.setEnabled(false);
+        verificar();
     }
 
     public void jugarNuevo(){
@@ -109,31 +76,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         txtv_ganador.setText(" ");
         txtv_res.setText("0");
 
-        btn1.setEnabled(true);
-        btn2.setEnabled(true);
-        btn3.setEnabled(true);
-        btn4.setEnabled(true);
-        btn5.setEnabled(true);
-
-        btn1.setBackgroundColor(Color.GRAY);
-        btn2.setBackgroundColor(Color.GRAY);
-        btn3.setBackgroundColor(Color.GRAY);
-        btn4.setBackgroundColor(Color.GRAY);
-        btn5.setBackgroundColor(Color.GRAY);
+        habilitarCartas();
     }
 
     public void verificar(){
 
-        if(res==21 || (intentos==5 && res<=21)){
+        if(!verificarIntentos() && res<=21){
 
             txtv_ganador.setText("Felicidades!!! Has ganado");
             txtv_ganador.setTextColor(Color.GREEN);
 
-            btn1.setEnabled(false);
-            btn2.setEnabled(false);
-            btn3.setEnabled(false);
-            btn4.setEnabled(false);
-            btn5.setEnabled(false);
+            deshabilitarCartas();
         }
 
         if(res>21){
@@ -141,11 +94,51 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             txtv_ganador.setText("Lo siento. Has perdido");
             txtv_ganador.setTextColor(Color.RED);
 
-            btn1.setEnabled(false);
-            btn2.setEnabled(false);
-            btn3.setEnabled(false);
-            btn4.setEnabled(false);
-            btn5.setEnabled(false);
+            deshabilitarCartas();
         }
+    }
+
+    public void habilitarCartas(){
+
+        int numCartas=ll_cartas.getChildCount();
+
+        for(int i=0; i<numCartas; i++){
+
+            View v=ll_cartas.getChildAt(i);
+            v.setEnabled(true);
+            v.setBackgroundColor(Color.GRAY);
+        }
+    }
+
+    public void deshabilitarCartas(){
+
+        int numCartas=ll_cartas.getChildCount();
+
+        for(int i=0; i<numCartas; i++){
+
+            View v=ll_cartas.getChildAt(i);
+            v.setEnabled(false);
+        }
+    }
+
+    public boolean verificarIntentos(){
+
+        boolean intentos=false;
+        int numCartas=ll_cartas.getChildCount();
+
+        for(int i=0; i<numCartas; i++){
+
+            View v=ll_cartas.getChildAt(i);
+
+            if(v instanceof  Button){
+
+                if(v.isEnabled()){
+
+                    intentos=true;
+                }
+            }
+
+        }
+        return intentos;
     }
 }
